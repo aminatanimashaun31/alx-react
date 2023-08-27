a4bfe05fddae66c80b539ff75e27ec09d0458c85
Aminat Animashaun <aminatanimashaun31@gmail.com>
2023-08-27 00:25:13 +0000

    react-state

---
+++ 0x05-react_state/task_0/dashboard/src/Header/Header.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import { shallow } from "enzyme";
+import { StyleSheetTestUtils } from "aphrodite";
+
+beforeEach(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+});
+afterEach(() => {
+  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+});
+
+describe("Header", () => {
+  it("render without crashing", () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.exists()).toEqual(true);
+  });
+  it("should render a h1", () => {
+    const wrapper = shallow(<Header />)
